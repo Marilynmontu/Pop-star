df94151de033ab9ca1192ce0b21e01ed53777cc8
liang hui <liang hui>
2016-04-19 22:40:31 +0800

    newbulid field

--- popstar/mainwindow.cpp
+++ popstar/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QPainter>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +11,21 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    const int GRID_SIZE = 40;
+    const int FIELD_SIZE = 9;
+    QPainter p(this);
+    p.setBrush(QBrush(Qt::red));
+    //p.drawRect(10, 10, 100, 100);
+       bool fill = true;
+       for (int i = 0; i < FIELD_SIZE*FIELD_SIZE; i++) {
+           int ix = i % FIELD_SIZE;
+           int iy = i / FIELD_SIZE;
+           if (fill) {
+               p.drawRect(ix * GRID_SIZE, iy * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+           }
+           fill = !fill;
+    }
 }
--- popstar/mainwindow.h
+++ popstar/mainwindow.h
@@ -17,6 +17,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void paintEvent(QPaintEvent*) override;
 };
 
 #endif // MAINWINDOW_H
