0a251ae3801009e6a42d545e6b96bc1e4af26378
liang hui <liang hui>
2016-04-19 23:42:51 +0800

    modified color

--- popstar/mainwindow.cpp
+++ popstar/mainwindow.cpp
@@ -22,15 +22,21 @@ void MainWindow::paintEvent(QPaintEvent *)
     const int GRID_SIZE = 40;
 
     QPainter p(this);
-    p.setBrush(QBrush(Qt::red));
+
 
     for (int ix = 0; ix < m_field->cols(); ix++) {
         for (int iy = 0; iy < m_field->rows(); iy++) {
            int color = m_field->grid(ix, iy);
-
-           if (color) {
-               p.drawRect(ix * GRID_SIZE, iy * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+           if(color==0) continue;
+           switch (color)
+           {
+           case 1:
+               p.setBrush(QBrush(Qt::red));break;
+           case 2:
+               p.setBrush(QBrush(Qt::green));break;
            }
+           p.drawRect(ix * GRID_SIZE, iy * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+
         }
     }
 }
